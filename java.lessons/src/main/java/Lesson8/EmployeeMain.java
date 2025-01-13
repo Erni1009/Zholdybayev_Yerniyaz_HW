18481dd3bb79c5d2b42e006e6983c7eed67424c1
Yerniyaz <borsteina@gmail.com>
2025-01-14 03:52:47 +0500

    Committing HW#8 Zholdybayev Yerniyaz.

--- java.lessons/src/main/java/Lesson8/EmployeeMain.java
+++ java.lessons/src/main/java/Lesson8/EmployeeMain.java
@@ -2,13 +2,13 @@ package Lesson8;
 
 public class EmployeeMain {
     public static void main(String[] args) {
-        Employee [] arrEmpl = new Employee[5];
-        arrEmpl[0]= new Employee("Alex","QA", "alex@alex",5533,44, 44);
-        arrEmpl[1]= new Employee("Mike","Dev", "mike@mike",5544,55, 33);
-        arrEmpl[2]= new Employee("Tim","CS", "tim@tim",5522,22, 22);
-        arrEmpl[3]= new Employee("Sam","QC", "sam@sam",5566,11, 55);
-        arrEmpl[4]= new Employee("Ben","Engineer", "ben@ben",5577,66, 36);
-        for(Employee employee1: arrEmpl) {
+        Employee [] arrEmp = new Employee[5];
+        arrEmp[0]= new Employee("Alex","QA", "alex@alex",5533,44, 44);
+        arrEmp[1]= new Employee("Mike","Dev", "mike@mike",5544,55, 33);
+        arrEmp[2]= new Employee("Tim","CS", "tim@tim",5522,22, 22);
+        arrEmp[3]= new Employee("Sam","QC", "sam@sam",5566,11, 55);
+        arrEmp[4]= new Employee("Ben","Engineer", "ben@ben",5577,66, 36);
+        for(Employee employee1: arrEmp) {
             if (employee1.getAge() < 40){
                 employee1.getInfo();
             }
